7f6a740604c456352fa6a7785b2566222044b584
Michal Cichra <michal@cichra.cz>
2016-09-13 14:35:49 +0200

    work in progress oauth

---
+++ src/authorize.lua
@@ -0,0 +1,119 @@
+local random = require 'resty.random'
+local ts = require 'threescale_utils'
+local redis = require 'resty.redis'
+local red = redis:new()
+
+-- returns a unique string for the client_id. it will be short lived
+local function nonce(client_id)
+  return ts.sha1_digest(tostring(random.bytes(20, true)) .. "#login:" .. client_id)
+end
+
+local function generate_access_token(client_id)
+  return ts.sha1_digest(tostring(random.bytes(20, true)) .. client_id)
+end
+
+-- As per RFC for Authorization Code flow: extract params from request uri
+-- If implementation deviates from RFC, this function should be over-ridden
+local function extract_params()
+  local params = {}
+  local uri_params = ngx.req.get_uri_args()
+
+  params.response_type = uri_params.response_type
+  params.client_id = uri_params.client_id
+  params.redirect_uri = uri_params.redirect_uri
+  params.scope =  uri_params.scope
+  params.state = uri_params.state
+
+  return params
+end
+
+local function persist_nonce(params)
+  local n = nonce(params.client_id)
+  params.state = n
+
+  ts.connect_redis(red)
+  local pre_token = generate_access_token(params.client_id)
+  params.tok = pre_token
+
+  local ok, err = red:hmset(ngx.var.service_id .. "#tmp_data:".. n,
+    {client_id = params.client_id,
+      redirect_uri = params.redirect_uri,
+      plan_id = params.scope,
+      access_token = pre_token})
+
+  if not ok then
+    ts.error(ts.dump(err))
+  end
+
+  return n, err
+end
+
+-- redirects_to the authorization url of the API provider with a secret
+-- 'state' which will be used when the form redirects the user back to
+-- this server.
+local function redirect_to_auth(params)
+
+  if not params.error then
+    persist_nonce(params)
+  end
+
+  local args = ts.build_query(params)
+
+  ngx.header.content_type = "application/x-www-form-urlencoded"
+  return ngx.redirect( ngx.var.auth_url .. "?" .. args )
+end
+
+-- Authorizes the client for the given scope
+local function authorize(params)
+  local required_params = {'client_id', 'redirect_uri', 'response_type', 'scope'}
+
+  if params["response_type"] ~= 'code' then
+    params.error = "unsupported_response_type"
+  elseif not ts.required_params_present(required_params, params) then
+    params.error = "invalid_request"
+  end
+
+  redirect_to_auth(params)
+end
+
+
+-- Check valid params ( client_id / secret / redirect_url, whichever are sent) against 3scale
+local function check_client_credentials(params)
+  local res = ngx.location.capture("/_threescale/check_credentials",
+    { args = { app_id = params.client_id, app_key = params.client_secret, redirect_uri = params.redirect_uri }})
+
+  if res.status ~= 200 then
+    params.error = "invalid_client"
+    redirect_to_auth(params)
+  end
+
+  return { ["status"] = res.status, ["body"] = res.body }
+end
+
+-- Check valid credentials
+local function check_credentials(params)
+  local res = check_client_credentials(params)
+  return res.status == 200
+end
+
+
+local _M = {
+  VERSION = '0.0.1'
+}
+
+function _M.call()
+  local params = extract_params()
+
+  local is_valid = check_credentials(params)
+
+  if is_valid then
+    authorize(params)
+  else
+    params.error = "invalid_client"
+    redirect_to_auth(params)
+  end
+end
+
+_M.persist_nonce = persist_nonce
+
+return _M
---
+++ src/get_token.lua
@@ -0,0 +1,137 @@
+local cjson = require 'cjson'
+local ts = require 'threescale_utils'
+local redis = require 'resty.redis'
+local red = redis:new()
+
+-- As per RFC for Authorization Code flow: extract params from Authorization header and body
+-- If implementation deviates from RFC, this function should be over-ridden
+local function extract_params()
+  local params = {}
+  local header_params = ngx.req.get_headers()
+
+  params.authorization = {}
+
+  if header_params['Authorization'] then
+    params.authorization = ngx.decode_base64(header_params['Authorization']:split(" ")[2]):split(":")
+  end
+
+  -- TODO: exit with 400 if the request is GET
+  ngx.req.read_body()
+  local body_params = ngx.req.get_post_args()
+
+  params.client_id = params.authorization[1] or body_params.client_id
+  params.client_secret = params.authorization[2] or body_params.client_secret
+
+  params.grant_type = body_params.grant_type
+  params.code = body_params.code
+  params.redirect_uri = body_params.redirect_uri or body_params.redirect_url
+
+  return params
+end
+
+-- Returns the access token (stored in redis) for the client identified by the id
+-- This needs to be called within a minute of it being stored, as it expires and is deleted
+local function request_token(params)
+  local ok, err = ts.connect_redis(red)
+  ok, err =  red:hgetall("c:".. params.code)
+
+  if ok[1] == nil then
+    return { ["status"] = 403, ["body"] = '{"error": "expired_code"}' }
+  else
+    local client_data = red:array_to_hash(ok)
+    params.user_id = client_data.user_id
+    if params.code == client_data.code then
+      return { ["status"] = 200, ["body"] = { ["access_token"] = client_data.access_token, ["token_type"] = "bearer", ["expires_in"] = 604800 } }
+    else
+      return { ["status"] = 403, ["body"] = '{"error": "invalid authorization code"}' }
+    end
+  end
+end
+
+
+
+-- Check valid params ( client_id / secret / redirect_url, whichever are sent) against 3scale
+local function check_client_credentials(params)
+  local res = ngx.location.capture("/_threescale/check_credentials",
+    { args = { app_id = params.client_id, app_key = params.client_secret, redirect_uri = params.redirect_uri },
+      copy_all_vars = true })
+
+  if res.status ~= 200 then
+    ngx.status = 401
+    ngx.header.content_type = "application/json; charset=utf-8"
+    ngx.print('{"error":"invalid_client"}')
+    ngx.exit(ngx.HTTP_OK)
+  end
+
+  return { ["status"] = res.status, ["body"] = res.body }
+end
+
+
+-- Returns the token to the client
+local function send_token(token)
+  ngx.header.content_type = "application/json; charset=utf-8"
+  ngx.say(cjson.encode(token))
+  ngx.exit(ngx.HTTP_OK)
+end
+
+-- Check valid credentials
+local function check_credentials(params)
+  local res = check_client_credentials(params)
+  return res.status == 200
+end
+
+-- Stores the token in 3scale. You can change the default ttl value of 604800 seconds (7 days) to your desired ttl.
+local function store_token(params, token)
+  local body = ts.build_query({ app_id = params.client_id, token = token.access_token, user_id = params.user_id, ttl = token.expires_in })
+  local stored = ngx.location.capture( "/_threescale/oauth_store_token", { method = ngx.HTTP_POST, body = body } )
+  stored.body = stored.body or stored.status
+  return { ["status"] = stored.status , ["body"] = stored.body }
+end
+
+
+-- Get the token from Redis
+local function get_token(params)
+  local required_params = {'client_id', 'client_secret', 'grant_type', 'code', 'redirect_uri'}
+
+  local res = {}
+
+  if ts.required_params_present(required_params, params) and params['grant_type'] == 'authorization_code'  then
+    res = request_token(params)
+  else
+    res = { ["status"] = 403, ["body"] = '{"error": "invalid_request"}' }
+  end
+
+  if res.status == 200 then
+    local token = res.body
+    local stored = store_token(params, token)
+
+    if stored.status == 200 then
+      send_token(token)
+    else
+      ngx.say('{"error":"'..stored.body..'"}')
+      ngx.exit(stored.status)
+    end
+  else
+    ngx.status = res.status
+    ngx.header.content_type = "application/json; charset=utf-8"
+    ngx.print(res.body)
+    ngx.exit(ngx.HTTP_FORBIDDEN)
+  end
+end
+
+local _M = {
+  VERSION = '0.0.1'
+}
+
+function _M.call()
+  local params = extract_params()
+
+  local is_valid = check_credentials(params)
+
+  if is_valid then
+    get_token(params)
+  end
+end
+
+return _M
+

f634e01cd7b7445f72acb7977b6fa53c5dc72217
Michal Cichra <michal@cichra.cz>
2017-01-03 20:00:19 +0100

    [http_ng] import apitools http_ng client
    https://github.com/APItools/monitor/pull/10
    
    APItools had quite advanced http client with custom backends
    and async requests. I hope to take that as a base to create smart
    http client that would understand caching and authentication.
    
    But also having custom backends would make it much easier to test and
    stub responses.
    
    [http_ng] cleanup the code to not use globals
    
    [http_ng] pass the luacheck linter

---
+++ apicast/src/resty/http_ng/response.lua
@@ -0,0 +1,56 @@
+local assert = assert
+local rawget = rawget
+local rawset = rawset
+local type = type
+local setmetatable = setmetatable
+------------
+-- @module middleware
+
+--- response
+-- @type HTTP
+
+--- Response returned by http.get and similar calls
+-- @table response
+-- @field[type=int] status HTTP Status Code
+-- @field[type=table] headers HTTP Headers
+-- @field[type=string] body response body
+-- @field[type=bool] ok
+
+
+local response = {}
+response.headers = require 'resty.http_ng.headers'
+
+
+function response.new(status, headers, body)
+  assert(status)
+  assert(body)
+
+  local mt = {}
+  mt['__index'] = function(table, key)
+    local generator = rawget(mt, key)
+    if generator then
+      rawset(table, key, generator(table))
+    end
+    return rawget(table, key)
+  end
+
+  local res = {
+    status = status,
+    headers = response.headers.new(headers),
+    ok = true
+  }
+
+  if type(body) == 'string' then
+    res.body = body
+  elseif type(body) == 'function' then
+    mt.body = body
+  end
+
+  return setmetatable(res, mt)
+end
+
+function response.error(message, request)
+  return { ok = false, error = message, request = request, status = 0, headers = {} }
+end
+
+return response

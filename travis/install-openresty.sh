a2320118dbf28de292a669222d485fa738098c46
Michal Cichra <michal@cichra.cz>
2017-01-09 10:47:11 +0100

    [travis] unify openresty version across docker and native

--- travis/install-openresty.sh
+++ travis/install-openresty.sh
@@ -2,15 +2,16 @@
 set -eux
 
 PREFIX=${1:-$HOME/openresty}
+OPENRESTY_ARCHIVE="openresty-${OPENRESTY_VERSION}"
 
 if [ ! -d "${PREFIX}/bin" ]; then
   cd /tmp/
-  wget -T 60 -q -c http://openresty.org/download/${OPENRESTY_VERSION}.tar.gz
-  tar -xzf ${OPENRESTY_VERSION}.tar.gz
-  rm -rf ${OPENRESTY_VERSION}.tar.gz
-  cd ${OPENRESTY_VERSION}
+  wget -T 60 -q -c "http://openresty.org/download/${OPENRESTY_ARCHIVE}.tar.gz"
+  tar -xzf "${OPENRESTY_ARCHIVE}.tar.gz"
+  rm -rf "${OPENRESTY_ARCHIVE}.tar.gz"
+  cd "${OPENRESTY_ARCHIVE}"
   ./configure --prefix="${PREFIX}" --with-ipv6 --with-luajit-xcflags=-DLUAJIT_ENABLE_LUA52COMPAT --with-debug
-  make -j$(cat /proc/cpuinfo  | grep -c processor)
+  make "-j$(grep -c processor /proc/cpuinfo)"
   make install
   ln -sf "${PREFIX}"/luajit/bin/luajit-* "${PREFIX}/luajit/bin/luajit"
   ln -sf "${PREFIX}"/luajit/include/luajit-* "${PREFIX}/luajit/include/lua5.1"

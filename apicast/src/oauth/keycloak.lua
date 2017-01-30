6447dd62a34d85e9908e3a292742a905b528806e
Daria Mayorova <d.mayorova@gmail.com>
2017-01-30 10:13:56 +0100

    Params checking functions for Keycloak
    Params checking functions for Keycloak

--- apicast/src/oauth/keycloak.lua
+++ apicast/src/oauth/keycloak.lua
@@ -10,39 +10,105 @@ local cjson = require 'cjson'
 
 local http_ng = require "resty.http_ng"
 local resty_url = require 'resty.url'
+local inspect = require 'inspect'
 
 local _M = {
   _VERSION = '0.1'
 }
-
 local mt = { __index = _M }
+
+-- Required params for each grant type and response type.
+_M.params = {
+  grant_type = {
+    ['authorization_code'] = {'client_id','redirect_uri','code'},
+    ['password'] = {'client_id','client_secret','username','password'},
+    ['client_credentials'] = {'client_id','client_secret'}
+  },
+  response_type = {
+    ['code'] = {'client_id','redirect_uri'},
+    ['token'] = {'client_id','redirect_uri'}
+  }
+}
+
+function _M.respond_and_exit(status, body, headers)
+  -- TODO: is there a better way to populate the response headers?..
+  if headers then
+    for name,value in pairs(headers) do
+      ngx.header[name] = value
+    end
+  end
+
+  ngx.status = status
+  ngx.print(body)
+  ngx.exit(ngx.HTTP_OK)
+end
+
+function _M.respond_with_error(status, message)
+  local headers = {
+    ['Content-Type'] = 'application/json;charset=UTF-8'
+  }
+  local body = '{"error":"'..message..'"}"'
+  _M.respond_and_exit(status, body, headers)
+end
+
+function _M.authorize_check_params(params)
+  local response_type = params['response_type']
+  local required_params = _M.params.response_type
+  if not response_type then return false, 'invalid_request' end
+  if not required_params[response_type] then return false, 'unsupported_response_type' end
+
+  for _,v in ipairs(required_params[response_type]) do
+    if not params[v] then
+      return false, 'invalid_request'
+    end
+  end
+  return true
+end
+
+function _M.token_check_params(params)
+  local grant_type = params['grant_type']
+  local required_params = _M.params.grant_type
+  if not grant_type then return false, 'invalid_request' end
+  if not required_params[grant_type] then return false, 'unsupported_grant_type' end
+
+  for _,v in ipairs(required_params[grant_type]) do
+    if not params[v] then
+      return false, 'invalid_request'
+    end
+  end
+  return true
+end
+
+function _M.check_client_id(client_id)
+  -- make a call to 3scale to verify the credentials
+  return true
+end
 
 function _M.authorize(self)
 
-  local http_client = self.http_client
+  local ok, err
+  local params = ngx.req.get_uri_args()
+  ok, err = _M.authorize_check_params(params)
 
-  if not http_client then
-    return nil, 'not initialized'
+  if err then
+    _M.repond_with_error(400, err)
   end
-
-  -- TODO: check request params and reply with error if something is wrong
 
   -- TODO: check client_id at 3scale to see if it's valid, reply with error if it's not
-
-  -- call Keycloak authorize
-  local url = resty_url.join(self.config.authorize_url,'?', ngx.var.args)
-
-  -- TODO: maybe use the same method the original request uses, not overriding to GET
-  local res = http_client.get(url)
-
-  -- TODO: is there a better way to populate the response headers?..
-  for name,value in pairs(res.headers) do
-    ngx.header[name] = value
+  local client_id = params.client_id
+  ok, err = _M.check_client_id(client_id)
+  if err then
+    _M.repond_with_error(401, 'invalid_client')
   end
 
-  ngx.status = res.status
-  ngx.print(res.body)
-  ngx.exit(ngx.HTTP_OK)
+  -- call Keycloak authorize
+  local args = ngx.var.args and '?'..ngx.var.args or ''
+  local url = resty_url.join(self.config.authorize_url, args)
+  
+  local http_client = self.http_client
+  local res = http_client.get(url)
+
+  _M.respond_and_exit(res.status, res.body, res.headers)
 end
 
 function _M.get_token(self)
@@ -65,13 +131,8 @@ function _M.get_token(self)
   local req_body = ngx.req.get_post_args()
 
   local res = http_client.post(url, req_body)
-  for name,value in pairs(res.headers) do
-    ngx.header[name] = value
-  end
 
-  ngx.status = res.status
-  ngx.print(res.body)
-  ngx.exit(ngx.HTTP_OK)
+  _M.respond_and_exit(res.status, res.body, res.headers)
 end
 
 function _M.callback()
@@ -117,7 +178,7 @@ end
 -- The format of the config file is the following:
 --{
 --  "type": "keycloak",
---  "server": "http://KEYCLOAK_HOST:8080",  -- TODO: https doesn't work even with ssl verify false, though the certificate should be OK
+--  "server": "http://KEYCLOAK_HOST:8080",
 --  "realm": "REALM_NAME",
 --  "initial_access_token": "FOR_CLIENT_REGISTRATION",
 --  "public_key": "FOR_VALIDATING_JWT"

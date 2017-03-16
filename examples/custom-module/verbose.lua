582342bd254e3f80ccc8db8c60945df1dd007542
Erwin Steffens <esteffens@rovecom.nl>
2017-03-16 13:50:01 +0100

    Fix the custom module example

--- examples/custom-module/verbose.lua
+++ examples/custom-module/verbose.lua
@@ -3,10 +3,14 @@ local apicast = require('apicast')
 local _M = { _VERSION = '0.0' }
 local mt = { __index = apicast }
 
+function _M.new()
+  return setmetatable(_M, { __index = apicast.new() })
+end
+
 function _M.log()
   ngx.log(ngx.WARN,
     'upstream response time: ', ngx.var.upstream_response_time, ' ',
     'upstream connect time: ', ngx.var.upstream_connect_time)
 end
 
-return setmetatable(_M, mt)
+return _M

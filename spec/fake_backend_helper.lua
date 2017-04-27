bb69f13bcb8e29802449d792451479ed7119d856
Michal Cichra <michal@cichra.cz>
2017-04-27 16:19:26 +0200

    [spec] fake http backend allows custom response

--- spec/fake_backend_helper.lua
+++ spec/fake_backend_helper.lua
@@ -1,12 +1,19 @@
+local type = type
+
 local fake_backend = {}
 
-function fake_backend.new()
+function fake_backend.new(response)
   local backend = { requests = {} }
 
   backend.send = function(_, request)
     backend.requests[#backend.requests + 1] = request
     backend.last_request = request
-    return { request = request, status = 200 }
+
+    if response then
+      return response(request)
+    else
+      return { request = request, status = 200 }
+    end
   end
 
   return backend

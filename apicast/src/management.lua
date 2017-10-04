c2c9e3b9dec8762742987e3745bab010426957e7
David Ortiz <z.david.ortiz@gmail.com>
2017-10-04 10:44:36 +0200

    management: set correct content-type in live & ready endpoints
    Both the ready and the live endpoints were returning 'text/plain', but
    the actual content is JSON.

--- apicast/src/management.lua
+++ apicast/src/management.lua
@@ -9,7 +9,7 @@ local inspect = require('inspect')
 local resolver_cache = require('resty.resolver.cache')
 local env = require('resty.env')
 
-local live = cjson.encode({status = 'live', success = true})
+local live = { status = 'live', success = true }
 
 local function json_response(body, status)
   ngx.header.content_type = 'application/json; charset=utf-8'
@@ -20,14 +20,11 @@ end
 function _M.ready()
   local status = _M.status()
   local code = status.success and ngx.HTTP_OK or 412
-
-  ngx.status = code
-  ngx.say(cjson.encode(status))
+  json_response(status, code)
 end
 
 function _M.live()
-  ngx.status = ngx.HTTP_OK
-  ngx.say(live)
+  json_response(live, ngx.HTTP_OK)
 end
 
 function _M.status(config)

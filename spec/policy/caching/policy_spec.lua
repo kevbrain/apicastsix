661b9df04ee0b38a11dcdbe6712177f30374c6a3
David Ortiz <z.david.ortiz@gmail.com>
2018-01-15 10:32:22 +0100

    policy/spec: add tests with disabled cache

--- spec/policy/caching/policy_spec.lua
+++ spec/policy/caching/policy_spec.lua
@@ -1,7 +1,30 @@
 local resty_lrucache = require('resty.lrucache')
 
 describe('policy', function()
-  describe('.export', function()
+  describe('.new', function()
+    local cache = resty_lrucache.new(1)
+
+    it('disables caching when caching type is not specified', function()
+      local caching_policy = require('apicast.policy.caching').new({})
+      local ctx = {}
+      caching_policy:rewrite(ctx)
+
+      ctx.cache_handler(cache, 'a_key', { status = 200 }, nil)
+      assert.is_nil(cache:get('a_key'))
+    end)
+
+    it('disables caching when invalid caching type is specified', function()
+      local config = { caching_type = 'invalid_caching_type' }
+      local caching_policy = require('apicast.policy.caching').new(config)
+      local ctx = {}
+      caching_policy:rewrite(ctx)
+
+      ctx.cache_handler(cache, 'a_key', { status = 200 }, nil)
+      assert.is_nil(cache:get('a_key'))
+    end)
+  end)
+
+  describe('.access', function()
     describe('when configured as strict', function()
       local caching_policy
       local cache
@@ -63,6 +86,25 @@ describe('policy', function()
 
         ctx.cache_handler(cache, 'a_key', { status = 500 }, nil)
         assert.equals(200, cache:get('a_key'))
+      end)
+    end)
+
+    describe('when disabled', function()
+      local caching_policy
+      local cache
+      local ctx
+
+      setup(function()
+        local config = { caching_type = 'none' }
+        caching_policy = require('apicast.policy.caching').new(config)
+        ctx = {}
+        caching_policy:rewrite(ctx)
+        cache = resty_lrucache.new(1)
+      end)
+
+      it('does not cache anything', function()
+        ctx.cache_handler(cache, 'a_key', { status = 200 }, nil)
+        assert.is_nil(cache:get('a_key'))
       end)
     end)
   end)

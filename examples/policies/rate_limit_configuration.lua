9584a8c1af31dbb8049c932a0723597fc5e9b73a
Michal Cichra <mikz@users.noreply.github.com>
2018-05-11 11:38:30 +0200

    Merge pull request #698 from 3scale/adapt-rate-limit-example-to-new-config
    Adapt rate limit policy example config to the new config schema

--- examples/policies/rate_limit_configuration.lua
+++ examples/policies/rate_limit_configuration.lua
@@ -1,26 +1,28 @@
 local policy_chain = require('apicast.policy_chain').default()
 
 local rate_limit_policy = require('apicast.policy.rate_limit').new({
-  limiters = {
-  {
-    name = "connections",
-    key = {name = "limit1"},
-    conn = 20,
-    burst = 10,
-    delay = 0.5
+  connection_limiters = {
+    {
+      key = {name = "limit1"},
+      conn = 20,
+      burst = 10,
+      delay = 0.5
+    }
   },
-  {
-    name = "leaky_bucket",
-    key = {name = "limit2"},
-    rate = 18,
-    burst = 9
+  leaky_bucket_limiters = {
+    {
+      key = {name = "limit2"},
+      rate = 18,
+      burst = 9
+    }
   },
-  {
-    name = "fixed_window",
-    key = {name = "limit3"},
-    count = 10,
-    window = 10
-  }},
+  fixed_window_limiters = {
+    {
+      key = {name = "limit3"},
+      count = 10,
+      window = 10
+    }
+  },
   redis_url = "redis://localhost:6379/1"
 })
 

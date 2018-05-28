c044c836d0ad7466a430418d28ad29b3f890a18b
Michal Cichra <michal@cichra.cz>
2018-05-28 15:53:39 +0200

    [busted] integrate luacov

--- bin/busted.lua
+++ bin/busted.lua
@@ -2,7 +2,16 @@ if ngx ~= nil then
   ngx.exit = function()end
 end
 
-pcall(require, 'luarocks.loader')
+local ok, luacov = pcall(require, 'luacov.runner')
+
+if ok then
+  local pwd = os.getenv('PWD')
+
+  for _, option in ipairs({"statsfile", "reportfile"}) do
+    -- properly expand current working dir, workaround for https://github.com/openresty/resty-cli/issues/35
+    luacov.defaults[option] = pwd .. package.config:sub(1, 1) .. luacov.defaults[option]
+  end
+end
 
 -- Busted command-line runner
 require 'busted.runner'({ standalone = false })

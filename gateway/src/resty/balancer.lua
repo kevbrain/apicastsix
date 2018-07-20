683e5a1eed17c54fd53155bc6ae3bb68185519d8
Michal Cichra <michal@cichra.cz>
2018-07-20 15:07:19 +0200

    [resty.balancer] add set_current_peer method
    It accepts only address and port.

--- gateway/src/resty/balancer.lua
+++ gateway/src/resty/balancer.lua
@@ -2,23 +2,25 @@ local setmetatable = setmetatable
 local insert = table.insert
 local tonumber = tonumber
 
-local ngx_balancer = require "ngx.balancer"
-
 local _M = {
   _VERSION = '0.1'
 }
 
 local mt = { __index = _M }
 
-function _M.new(mode)
-  if not mode then
-    return nil, 'missing balancing function'
-  end
+do
+  local ngx_balancer = require "ngx.balancer"
 
-  return setmetatable({
-    mode = mode,
-    balancer = ngx_balancer
-  }, mt)
+  function _M.new(mode)
+    if not mode then
+      return nil, 'missing balancing function'
+    end
+
+    return setmetatable({
+      mode = mode,
+      balancer = ngx_balancer
+    }, mt)
+  end
 end
 
 local function new_peer(server, port)
@@ -87,6 +89,31 @@ function _M.select_peer(self, peers)
   return peer
 end
 
+function _M:set_current_peer(address, port)
+  local ngx_balancer = self.balancer
+
+  if not ngx_balancer then
+    return nil, 'balancer not available'
+  end
+
+  if not address then
+    return nil, 'peer missing address'
+  end
+
+  if not port then
+    return nil, 'peer missing port'
+  end
+
+  local ok, err = ngx_balancer.set_current_peer(address, port)
+  ngx.log(ngx.INFO, 'balancer set peer ', address, ':', port, ' ok: ', ok, ' err: ', err)
+
+  if ok then
+    return true
+  else
+    return nil, err or 'balancer could not set the peer'
+  end
+end
+
 function _M.set_peer(self, peers)
   local balancer = self.balancer
 
@@ -95,25 +122,19 @@ function _M.set_peer(self, peers)
   end
 
   local peer, err = self:select_peer(peers)
-  local address, port, ok
 
-  if not peer then
+  if peer then
+    local ok
+    ok, err = self:set_current_peer(peer[1], peer[2])
+
+    if ok then
+      return peer
+    else
+      return nil, err
+    end
+  else
     return nil, err or 'no peer found'
   end
-
-  address, port = peer[1], peer[2]
-
-  if not address or not port then
-    return nil, 'peer missing address or port'
-  end
-
-  ngx.log(ngx.INFO, 'balancer set peer ', address, ':', port)
-
-  ok, err = balancer.set_current_peer(address, port)
-
-  if ok then return peer end
-
-  return nil, err or 'balancer could not set the peer'
 end
 
 return _M

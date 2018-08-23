3daad7c1f5492d623f22dd343fd8b5dc245697d3
Michal Cichra <michal@cichra.cz>
2018-08-23 16:58:47 +0200

    [oidc] implemented resty.oidc.discovery
    Load configuration via OIDC Discovery endpoint.

---
+++ gateway/src/resty/oidc/discovery.lua
@@ -0,0 +1,110 @@
+local setmetatable = setmetatable
+local type = type
+local len = string.len
+
+local resty_url = require 'resty.url'
+local http_ng = require "resty.http_ng"
+local resty_env = require 'resty.env'
+local Mime = require 'resty.mime'
+local cjson = require('cjson')
+local jwk = require('resty.oidc.jwk')
+
+local oidc_log_level = ngx[string.upper(resty_env.value('APICAST_OIDC_LOG_LEVEL') or 'err')] or ngx.ERR
+
+local _M = { }
+
+local mt = { __index = _M }
+
+local function openid_configuration_url(issuer)
+    if issuer and type(issuer) == 'string' and len(issuer) > 0 then
+        return resty_url.join(issuer, '.well-known/openid-configuration')
+    end
+end
+
+local function mime_type(content_type)
+    return Mime.new(content_type).media_type
+end
+
+local function decode_json(response)
+    if mime_type(response.headers.content_type) == 'application/json' then
+        return cjson.decode(response.body)
+    else
+        return nil, 'not json'
+    end
+end
+
+function _M.new(http_backend)
+    local http_client = http_ng.new{
+        backend = http_backend,
+        options = {
+            ssl = { verify = resty_env.enabled('OPENSSL_VERIFY') }
+        }
+    }
+
+    local self = { http_client = http_client }
+
+    return setmetatable(self, mt)
+end
+
+--- Fetch and return OIDC configuration from well known endpoint <issuer>/.well-known/openid-configuration
+-- @tparam string issuer URL to the Issuer (without the .well-known/openid-configuration)
+function _M:openid_configuration(issuer)
+    local http_client = self.http_client
+
+    if not http_client then
+        return nil, 'not initialized'
+    end
+
+    local uri = openid_configuration_url(issuer)
+
+    if not uri then
+        return nil, 'no OIDC endpoint'
+    end
+
+    local res = http_client.get(uri)
+
+    if res.status ~= 200 then
+        ngx.log(oidc_log_level, 'failed to get OIDC Provider from ', uri, ' status: ', res.status, ' body: ', res.body)
+        return nil, 'could not get OpenID Connect configuration'
+    end
+
+    local config = decode_json(res)
+
+    if not config then
+        ngx.log(oidc_log_level, 'invalid OIDC Provider, expected application/json got:  ', res.headers.content_type, ' body: ', res.body)
+        return nil, 'invalid JSON'
+    end
+
+    return config
+end
+
+--- Fetch and convert JWK keys. Each key will get .pem property with PEM formatted key.
+-- @tparam table configuration OIDC configuration from :openid_configuration
+-- @treturn table list of JWK keys
+function _M:jwks(configuration)
+    local http_client = self.http_client
+
+    if not http_client then
+        return nil, 'not initialized'
+    end
+
+    if not configuration then
+        return nil, 'no config'
+    end
+
+    local jwks_uri = configuration.jwks_uri
+
+    if not jwks_uri or jwks_uri == ngx.null then
+        return nil, 'no jwks_uri'
+    end
+
+    local res = http_client.get(jwks_uri)
+
+    if res.status == 200 then
+        return jwk.convert_keys(decode_json(res))
+    else
+        return nil, 'invalid response'
+    end
+end
+
+return _M
---
+++ spec/resty/oidc/discovery_spec.lua
@@ -0,0 +1,137 @@
+local test_backend_client = require 'resty.http_ng.backend.test'
+local _M = require('resty.oidc.discovery')
+local cjson = require('cjson')
+
+describe('OIDC Discovery', function()
+    local test_backend
+    before_each(function() test_backend = test_backend_client.new() end)
+
+    local discovery
+    before_each(function() discovery = _M.new(test_backend) end)
+
+    describe('.new', function()
+        it('has .http_client', function()
+            assert(_M.new().http_client,'has .http_client')
+        end)
+    end)
+
+    describe(':openid_configuration(endpoint)', function()
+        it('loads configuration from the discovery endpoint', function()
+            test_backend
+                .expect{ url = "https://idp.example.com/auth/realms/foo/.well-known/openid-configuration" }
+                .respond_with{ status = 200, headers = { content_type = 'application/json' }, body = [[
+                    {
+                      "issuer": "https://idp.example.com/auth/realms/foo",
+                      "jwks_uri": "https://idp.example.com/auth/realms/foo/jwks",
+                      "id_token_signing_alg_values_supported": [ "RS256" ]
+                    }
+                  ]] }
+
+            local config = assert(discovery:openid_configuration('https://idp.example.com/auth/realms/foo'))
+
+            assert.same({
+                id_token_signing_alg_values_supported = { 'RS256' },
+                issuer = 'https://idp.example.com/auth/realms/foo',
+                jwks_uri = 'https://idp.example.com/auth/realms/foo/jwks',
+            }, config)
+        end)
+
+        it('returns error when config is not JSON', function()
+            test_backend
+                    .expect{ url = "https://idp.example.com/.well-known/openid-configuration" }
+                    .respond_with{ status = 200, headers = { content_type = 'text/plain' }, body = '{}' }
+
+            assert.returns_error('invalid JSON', discovery:openid_configuration('https://idp.example.com'))
+        end)
+
+        it('returns error when status is not 200', function()
+            test_backend
+                    .expect{ url = "https://idp.example.com/.well-known/openid-configuration" }
+                    .respond_with{ status = 201, headers = { content_type = 'text/plain' }, body = '{}' }
+
+            assert.returns_error('could not get OpenID Connect configuration', discovery:openid_configuration('https://idp.example.com'))
+        end)
+    end)
+
+    describe(':jwks(config)', function()
+
+        it('loads and decodes keys from jwks_uri endpoint', function()
+            local config = { jwks_uri = 'https://idp.example.com/auth/realms/foo/jwks' }
+            test_backend
+                .expect{ url = config.jwks_uri }
+                .respond_with{
+                    status = 200,
+                    headers = { content_type = 'application/json' },
+                    body =  [[ { "keys": [{
+                      "kid": "3g-I9PWt6NrznPLcbE4zZrakXar27FDKEpqRPlD2i2Y",
+                      "kty": "RSA",
+                      "n": "iqXwBiZgN2q1dCKU1P_vzyiGacdQhfqgxQST7GFlWU_PUljV9uHrLOadWadpxRAuskNpXWsrKoU_hDxtSpUIRJj6hL5YTlrvv-IbFwPNtD8LnOfKL043_ZdSOe3aT4R4NrBxUomndILUESlhqddylVMCGXQ81OB73muc9ovR68Ajzn8KzpU_qegh8iHwk-SQvJxIIvgNJCJTC6BWnwS9Bw2ns0fQOZZRjWFRVh8BjkVdqa4vCAb6zw8hpR1y9uSNG-fqUAPHy5IYQaD8k8QX0obxJ0fld61fH-Wr3ENpn9YZWYBcKvnwLm2bvxqmNVBzW4rhGEZb9mf-KrSagD5GUw",
+                      "e": "AQAB"
+                    }] } ]]
+                }
+
+            local keys = assert(discovery:jwks(config))
+
+            assert.same({ ['3g-I9PWt6NrznPLcbE4zZrakXar27FDKEpqRPlD2i2Y'] = {
+                e = 'AQAB',
+                kid = '3g-I9PWt6NrznPLcbE4zZrakXar27FDKEpqRPlD2i2Y',
+                kty = 'RSA',
+                n = 'iqXwBiZgN2q1dCKU1P_vzyiGacdQhfqgxQST7GFlWU_PUljV9uHrLOadWadpxRAuskNpXWsrKoU_hDxtSpUIRJj6hL5YTlrvv-IbFwPNtD8LnOfKL043_ZdSOe3aT4R4NrBxUomndILUESlhqddylVMCGXQ81OB73muc9ovR68Ajzn8KzpU_qegh8iHwk-SQvJxIIvgNJCJTC6BWnwS9Bw2ns0fQOZZRjWFRVh8BjkVdqa4vCAb6zw8hpR1y9uSNG-fqUAPHy5IYQaD8k8QX0obxJ0fld61fH-Wr3ENpn9YZWYBcKvnwLm2bvxqmNVBzW4rhGEZb9mf-KrSagD5GUw',
+                pem = [[
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiqXwBiZgN2q1dCKU1P/v
+zyiGacdQhfqgxQST7GFlWU/PUljV9uHrLOadWadpxRAuskNpXWsrKoU/hDxtSpUI
+RJj6hL5YTlrvv+IbFwPNtD8LnOfKL043/ZdSOe3aT4R4NrBxUomndILUESlhqddy
+lVMCGXQ81OB73muc9ovR68Ajzn8KzpU/qegh8iHwk+SQvJxIIvgNJCJTC6BWnwS9
+Bw2ns0fQOZZRjWFRVh8BjkVdqa4vCAb6zw8hpR1y9uSNG+fqUAPHy5IYQaD8k8QX
+0obxJ0fld61fH+Wr3ENpn9YZWYBcKvnwLm2bvxqmNVBzW4rhGEZb9mf+KrSagD5G
+UwIDAQAB
+-----END PUBLIC KEY-----
+]],
+            } }, keys)
+        end)
+
+        it('can process ForgeRock response', function()
+            local config = { jwks_uri = 'https://idp.example.com/auth/realms/foo/jwks' }
+            test_backend
+                .expect{ url = config.jwks_uri }
+                .respond_with{
+                    status = 200,
+                    headers = { content_type = 'application/json' },
+                    body = fixture('oidc', 'jwk', 'forgerock.json')
+                }
+
+            local keys = assert(discovery:jwks(config))
+
+            assert.same(cjson.decode(fixture('oidc', 'jwk', 'forgerock.apicast.json')),
+                    keys)
+        end)
+
+        it('ignores empty jwks_uri', function()
+            assert.returns_error('no jwks_uri', discovery:jwks{})
+            assert.returns_error('no jwks_uri', discovery:jwks{ jwks_uri = ngx.null })
+        end)
+
+        it('ignores empty config', function()
+            assert.returns_error('no config', discovery:jwks())
+        end)
+
+        it('returns error on invalid response', function()
+            local config = { jwks_uri = 'https://idp.example.com/auth/realms/foo/jwks' }
+            test_backend
+                .expect{ url = config.jwks_uri }
+                .respond_with{ status = 201 }
+
+            assert.returns_error('invalid response', discovery:jwks(config))
+        end)
+
+        it('returns error on not json response', function()
+            local config = { jwks_uri = 'https://idp.example.com/auth/realms/foo/jwks' }
+            test_backend
+                    .expect{ url = config.jwks_uri }
+                    .respond_with{ status = 200, body = '' }
+
+            assert.returns_error('not json', discovery:jwks(config))
+        end)
+    end)
+end)

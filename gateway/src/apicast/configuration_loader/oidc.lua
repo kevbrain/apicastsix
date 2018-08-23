a7e494fcf61d717b6a318b2b98839fb7b3967bf2
Michal Cichra <michal@cichra.cz>
2018-08-23 17:07:27 +0200

    [configuration] implement OIDC configuration loader
    This loader can be used to decorate existing loaders to add OIDC
    information.

---
+++ gateway/src/apicast/configuration_loader/oidc.lua
@@ -0,0 +1,54 @@
+--- This module is responsible for loading OIDC configuration via OIDC Discovery.
+--- Discovered configuration is put back to the APIcast configuration and serialized to JSON.
+
+local configuration_parser = require('apicast.configuration_parser')
+local cjson = require('cjson')
+
+local ipairs = ipairs
+local select = select
+local setmetatable = setmetatable
+
+local _M = {}
+
+
+local empty = {}
+
+local function array(table)
+    return setmetatable(table or {}, cjson.empty_array_mt)
+end
+
+_M.discovery = require('resty.oidc.discovery').new()
+
+local function load_oidc(issuer)
+    local config = _M.discovery:openid_configuration(issuer)
+    local keys = _M.discovery:jwks(config)
+
+    return { issuer = issuer, config = config, keys = keys }
+end
+
+local function load_service(service)
+    if not service or not service.proxy then return nil end
+
+    return load_oidc(service.proxy.oidc_issuer_endpoint)
+end
+
+function _M.call(...)
+    local contents = select(1, ...)
+    local config = configuration_parser.decode(contents)
+
+    if config then
+        local oidc = array(config.oidc)
+
+        for i,service in ipairs(config.services or empty) do
+            oidc[i] = oidc[i] or load_service(service)
+        end
+
+        config.oidc = oidc
+
+        return cjson.encode(config), select(2, ...)
+    else
+        return ...
+    end
+end
+
+return _M
---
+++ spec/configuration_loader/oidc_spec.lua
@@ -0,0 +1,58 @@
+local test_backend_client = require 'resty.http_ng.backend.test'
+local loader = require 'apicast.configuration_loader.oidc'
+local cjson = require('cjson')
+
+describe('OIDC Configuration loader', function()
+  describe('.call', function()
+    local test_backend
+    before_each(function() test_backend = test_backend_client.new() end)
+    before_each(function() loader.discovery.http_client.backend = test_backend end)
+
+    it('ignores empty config', function()
+      assert.same({}, { loader.call() })
+      assert.same({''}, { loader.call('') })
+    end)
+
+    it('ignores config without oidc_issuer_endpoint', function()
+      local config = cjson.encode{
+        services = {
+          { id = 21 },
+          { id = 42 },
+        }
+      }
+
+      assert(loader.call(config))
+    end)
+
+    it('forwards all parameters', function()
+      assert.same({'{"oidc":[]}', 'one', 'two'}, { loader.call('{}', 'one', 'two')})
+    end)
+
+    it('gets openid configuration', function()
+      local config = {
+        services = {
+          { id = 21, proxy = { oidc_issuer_endpoint = 'https://example.com' } },
+        }
+      }
+
+      test_backend
+        .expect{ url = "https://example.com/.well-known/openid-configuration" }
+        .respond_with{
+          status = 200,
+          headers = { content_type = 'application/json' },
+          body = [[{"jwks_uri":"http://example.com/jwks"}]],
+        }
+
+      test_backend
+        .expect{ url = "http://example.com/jwks" }
+        .respond_with{
+          status = 200,
+          headers = { content_type = 'application/json' },
+          body = [[{"keys":[]}]],
+        }
+      local oidc = loader.call(cjson.encode(config))
+
+      assert.same([[{"services":[{"id":21,"proxy":{"oidc_issuer_endpoint":"https:\/\/example.com"}}],"oidc":[{"issuer":"https:\/\/example.com","config":{"jwks_uri":"http:\/\/example.com\/jwks"},"keys":{}}]}]], oidc)
+    end)
+  end)
+end)

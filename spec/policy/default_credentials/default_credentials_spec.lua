3a66eb0e8be639f9925730277d06d50a6d34543c
David Ortiz <z.david.ortiz@gmail.com>
2018-11-16 13:09:55 +0100

    policy/default_credentials: store default credentials in context
    We can't store them in the service, because it's reused between requests
    for the same service.
    We need to store the credentials in the context, so they're not reused
    between requests.

--- spec/policy/default_credentials/default_credentials_spec.lua
+++ spec/policy/default_credentials/default_credentials_spec.lua
@@ -81,45 +81,45 @@ describe('Default credentials policy', function()
     describe('when the service in the context has backend_version = 1', function()
       describe('and the request includes a user key', function()
         describe('and there is a default user key defined', function()
-          it('does not set a user key in the service creds', function()
+          it('does not set any credentials in the context', function()
             local context = { service = service_with_user_key_in_req() }
 
             policy_with_default_user_key():rewrite(context)
 
-            assert.is_nil(context.service.extracted_credentials)
+            assert.is_nil(context.extracted_credentials)
           end)
         end)
 
         describe('and there is not a default user key defined', function()
-          it('does not set a user key in the service creds', function()
+          it('does not set any credentials in the context', function()
             local context = { service = service_with_user_key_in_req() }
 
             policy_with_default_app_id_and_key():rewrite(context)
 
-            assert.is_nil(context.service.extracted_credentials)
+            assert.is_nil(context.extracted_credentials)
           end)
         end)
       end)
 
       describe('and the request does not include a user key', function()
         describe('and there is a default user key defined', function()
-          it('sets the default user key in the service creds', function()
+          it('sets the default user key in the context', function()
             local context = { service = service_without_user_key_in_req() }
 
             policy_with_default_user_key():rewrite(context)
 
             assert.same({ policy_user_key, user_key = policy_user_key },
-                        context.service.extracted_credentials)
+                        context.extracted_credentials)
           end)
         end)
 
         describe('and there is not a default user key defined', function()
-          it('does not set a user key in the service creds', function()
+          it('does not set any credentials in the context', function()
             local context = { service = service_without_user_key_in_req() }
 
             policy_with_default_app_id_and_key():rewrite(context)
 
-            assert.is_nil(context.service.extracted_credentials)
+            assert.is_nil(context.extracted_credentials)
           end)
         end)
       end)
@@ -128,73 +128,73 @@ describe('Default credentials policy', function()
     describe('when the service in the context has backend_version = 2', function()
       describe('and the request includes an app id and an app key', function()
         describe('and there is a default app id and key defined', function()
-          it('does not set app id and key in the service creds', function()
+          it('does not set any credentials in the context', function()
             local context = { service = service_with_app_id_and_key_in_req() }
 
             policy_with_default_app_id_and_key():rewrite(context)
 
-            assert.is_nil(context.service.extracted_credentials)
+            assert.is_nil(context.extracted_credentials)
           end)
         end)
 
         describe('and there is not a default app and key defined', function()
-          it('does not set app id and key in the service creds', function()
+          it('does not set any credentials in the context', function()
             local context = { service = service_with_app_id_and_key_in_req() }
 
             policy_with_default_user_key():rewrite(context)
 
-            assert.is_nil(context.service.extracted_credentials)
+            assert.is_nil(context.extracted_credentials)
           end)
         end)
       end)
 
       describe('and the request includes an app id but no app key', function()
         describe('and there is a default app id and key defined', function()
-          it('does not set app id and key in the service creds', function()
+          it('does not set any credentials in the context', function()
             local context = { service = service_with_app_id_in_req() }
 
             policy_with_default_app_id_and_key():rewrite(context)
 
-            assert.is_nil(context.service.extracted_credentials)
+            assert.is_nil(context.extracted_credentials)
           end)
         end)
 
         describe('and there is not a default app and key defined', function()
-          it('does not set app id and key in the service creds', function()
+          it('does not set any credentials in the context', function()
             local context = { service = service_with_app_id_in_req() }
 
             policy_with_default_user_key():rewrite(context)
 
-            assert.is_nil(context.service.extracted_credentials)
+            assert.is_nil(context.extracted_credentials)
           end)
         end)
       end)
 
       describe('and the request includes an app key but not an app id', function()
         describe('and there is a default app id and key defined', function()
-          it('does not set app id and key in the service creds', function()
+          it('does not set any credentials in the context', function()
             local context = { service = service_with_app_key_in_req() }
 
             policy_with_default_app_id_and_key():rewrite(context)
 
-            assert.is_nil(context.service.extracted_credentials)
+            assert.is_nil(context.extracted_credentials)
           end)
         end)
 
         describe('and there is not a default app and key defined', function()
-          it('does not set app id and key in the service creds', function()
+          it('does not set any credentials in the context', function()
             local context = { service = service_with_app_key_in_req() }
 
             policy_with_default_user_key():rewrite(context)
 
-            assert.is_nil(context.service.extracted_credentials)
+            assert.is_nil(context.extracted_credentials)
           end)
         end)
       end)
 
       describe('and the request does not include an app key nor an app id', function()
         describe('and there is a default app id and key defined', function()
-          it('sets the app and key in the service creds', function()
+          it('sets the app and key in the context', function()
             local context = { service = service_without_app_id_nor_key_in_req() }
 
             policy_with_default_app_id_and_key():rewrite(context)
@@ -202,25 +202,25 @@ describe('Default credentials policy', function()
             assert.same(
               { policy_app_id, policy_app_key,
                 app_id = policy_app_id, app_key = policy_app_key },
-              context.service.extracted_credentials
+              context.extracted_credentials
             )
           end)
         end)
 
         describe('and there is not a default app and key defined', function()
-          it('does not set app id and key in the service creds', function()
+          it('does not set any credentials in the context', function()
             local context = { service = service_without_app_id_nor_key_in_req() }
 
             policy_with_default_user_key():rewrite(context)
 
-            assert.is_nil(context.service.extracted_credentials)
+            assert.is_nil(context.extracted_credentials)
           end)
         end)
       end)
     end)
 
     describe('when the service in the context has backend_version != 1 and != 2', function()
-      it('does not set app id and key in the service creds', function()
+      it('does not set any credentials in the context', function()
         local context = { service = { backend_version = 'oauth' } }
 
         local policies = {
@@ -230,7 +230,7 @@ describe('Default credentials policy', function()
 
         for _, policy in ipairs(policies) do
           policy:rewrite(context)
-          assert.is_nil(context.service.extracted_credentials)
+          assert.is_nil(context.extracted_credentials)
         end
       end)
     end)

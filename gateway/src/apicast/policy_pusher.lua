a860367eb290884d10fc5053e6155ddf48946cd1
David Ortiz <z.david.ortiz@gmail.com>
2019-03-11 18:28:35 +0100

    policy_pusher: send access token correctly
    Notice that there was a 'token:url' in the code and the tests instead of
    'token@url'. While at it, I've changed this to send it via params
    because that makes the tests a bit easier (do not need to convert things
    to base64) and it's closer to what the system UI shows because
    'access_token' appears as a param there.

--- gateway/src/apicast/policy_pusher.lua
+++ gateway/src/apicast/policy_pusher.lua
@@ -17,15 +17,16 @@ end
 
 local system_endpoint = '/admin/api/registry/policies'
 
-local function system_url(access_token, admin_portal_domain)
-  return format('https://%s:%s%s', access_token, admin_portal_domain, system_endpoint)
+local function system_url(admin_portal_domain)
+  return format('https://%s%s', admin_portal_domain, system_endpoint)
 end
 
 local function push_to_system(name, version, manifest, admin_portal_domain, access_token, http_client)
-  local url = system_url(access_token, admin_portal_domain)
+  local url = system_url(admin_portal_domain)
 
   return http_client.json.post(
-    url, { name = name, version = version, schema = manifest }
+    url,
+    { access_token = access_token, name = name, version = version, schema = manifest }
   )
 end
 
